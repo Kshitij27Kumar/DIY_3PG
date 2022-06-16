e44e193254e8e7b02604481985e7bdea8edf835d
Kshitij27Kumar <54733760+Kshitij27Kumar@users.noreply.github.com>
2022-06-16 15:10:31 +0530

    Add files via upload

---
+++ Week11_3pg/Week11_3pg.js
@@ -0,0 +1,20 @@
+//Level 1
+//1
+import fetch from 'node-fetch';
+const url = 'https://restcountries.eu/rest/v2/all'
+const fetchData = async () => {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        countries = data;
+        //logging the country name,capital,languages,population
+        countries.forEach(country => {
+            console.log(`country:${country.name} capital:${country.capital} languages:${country.languages} population${country.population}`)
+        })
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+console.log('===== async and await')
+fetchData();
---
+++ Week11_3pg/main.js
@@ -0,0 +1,2 @@
+import countries from "./countries";
+console.log(countries)
